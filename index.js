195e6831eb7060bf6ac6a49cb1ffbfcaecf3aff1
David <davidobob@laposte.net>
2020-08-08 12:49:32 +0200

    helmet & morgan added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const helmet = require("helmet");
+const morgan = require("morgan");
 const Joi = require("joi");
 const logger = require("./logger");
 const express = require("express");
@@ -6,6 +8,8 @@ const app = express();
 app.use(express.json());
 
 app.use(logger);
+app.use(helmet());
+app.use(morgan("tiny"));
 
 const courses = [
   { id: 1, name: "course 1" },
